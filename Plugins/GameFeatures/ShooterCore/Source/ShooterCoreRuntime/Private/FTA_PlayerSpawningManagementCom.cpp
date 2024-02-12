d8e389e182e603f6390a3b5b07116cca75a79db7
Ray.Tang <kaminaritukane@163.com>
2024-02-12 17:20:32 +0800

    Choose the farest playerstart from any players while spawning

--- Plugins/GameFeatures/ShooterCore/Source/ShooterCoreRuntime/Private/FTA_PlayerSpawningManagementCom.cpp
+++ Plugins/GameFeatures/ShooterCore/Source/ShooterCoreRuntime/Private/FTA_PlayerSpawningManagementCom.cpp
@@ -3,6 +3,8 @@
 
 #include "FTA_PlayerSpawningManagementCom.h"
 #include "Player/LyraPlayerStart.h"
+#include "GameModes/LyraGameState.h"
+#include "GameFramework/PlayerState.h"
 
 UFTA_PlayerSpawningManagementCom::UFTA_PlayerSpawningManagementCom(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -11,9 +13,53 @@ UFTA_PlayerSpawningManagementCom::UFTA_PlayerSpawningManagementCom(const FObject
 
 AActor* UFTA_PlayerSpawningManagementCom::OnChoosePlayerStart(AController* Player, TArray<ALyraPlayerStart*>& PlayerStarts)
 {
-	if (PlayerStarts.Num() > 0)
-		return PlayerStarts[0];
-	return nullptr;
+	ALyraGameState* GameState = GetGameStateChecked<ALyraGameState>();
+
+	// Find the player start which is farest from any players
+	ALyraPlayerStart* BestPlayerStart = nullptr;
+	double MaxSqrDistance = 0.f;
+	ALyraPlayerStart* FallbackPlayerStart = nullptr;
+	double FallbackMaxSqrDistance = 0;
+
+	for (ALyraPlayerStart* PlayerStart : PlayerStarts)
+	{
+		for (APlayerState* PS : GameState->PlayerArray)
+		{
+			if (PS->IsOnlyASpectator() || PS->GetOwningController() == Player)
+			{
+				continue;
+			}
+
+			if (APawn* Pawn = PS->GetPawn())
+			{
+				const double SqrDistance = PlayerStart->GetSquaredDistanceTo(Pawn);
+
+				if (PlayerStart->IsClaimed())
+				{
+					if (FallbackPlayerStart == nullptr || SqrDistance > FallbackMaxSqrDistance)
+					{
+						FallbackPlayerStart = PlayerStart;
+						FallbackMaxSqrDistance = SqrDistance;
+					}
+				}
+				else if (PlayerStart->GetLocationOccupancy(Player) < ELyraPlayerStartLocationOccupancy::Full)
+				{
+					if (BestPlayerStart == nullptr || SqrDistance > MaxSqrDistance)
+					{
+						BestPlayerStart = PlayerStart;
+						MaxSqrDistance = SqrDistance;
+					}
+				}
+			}
+		}
+	}
+
+	if (BestPlayerStart)
+	{
+		return BestPlayerStart;
+	}
+
+	return FallbackPlayerStart;
 }
 
 void UFTA_PlayerSpawningManagementCom::OnFinishRestartPlayer(AController* Player, const FRotator& StartRotation)
